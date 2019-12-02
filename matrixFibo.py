2c3eea34e08f3ef7bf62c20c5d3a88680603ae00
beltsysazar <alternate.3401@gmail.com>
2019-12-02 16:12:18 +0700

    Update matrixFibo.py

--- matrixFibo.py
+++ matrixFibo.py
@@ -1,14 +1,27 @@
-class ListMatrix: # assuming the matrices are 1-indexed, might implement asserting later
-    def __init__(self, matrix_list):
-        self.matrix_list = matrix_list
+# Still does not work, I might be dumb
+
+class ListMatrix: # assuming the matrices are 0-indexed, might implement asserting later
+    def __init__(self, matrix_list = []):
         # horizontal elements of the matrix
         self.horizontal = matrix_list
         # vertical elements of the matrix
-        self.vertical = [[i[j] for i in self.matrix_list] for j in range(len(self.matrix_list[0]))]
+        self.vertical = [[i[j] for i in matrix_list] for j in range(len(matrix_list[0]))]
     # getters for each element specific on the list
-    def get_hor(self, index):
-        return self.matrix_list[index - 1]
-    def get_ver(self, index):
-        return self.vertical[index - 1]
-    def multiply(self, othermatrix):
-        
+    def get_hor(self, index=0):
+        return self.horizontal[index]
+    def get_ver(self, index=0):
+        return self.vertical[index]
+    def multiply(self, other): # assuming the matrices don't violate any conventions
+        res_matrix = [[0] * len(self.horizontal) for _ in len(other.vertical)]
+        for row in range(len(res_matrix)):
+            for cell in range(row):
+                res_matrix[cell] += sum(self_ele * other_ele for self_ele, other_ele in zip(self.get_ver[row], other.get_hor[cell]))
+    def power(self, exponent = 1):
+        if exponent == 1:
+            return self.horizontal
+        else:
+            return self.multiply(self.power(exponent - 1))
+
+if __name__ == "__main__":
+    fibo = ListMatrix([[1, 1], [1, 0]])
+    print(fibo.power(2))

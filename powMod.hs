f411e795a1aee11e9d671eb79f2d40fc93489a64
beltsysazar <alternate.3401@gmail.com>
2020-09-16 18:02:44 +0700

    Realize that 0 and 1 are not the same thing and that I am incredibly stupid

--- powMod.hs
+++ powMod.hs
@@ -1,15 +1,6 @@
---this doesn't work
+--this now works!!!!
 powMod :: Integer -> Integer -> Integer -> Integer -> Integer
 powMod base exponent modulus return
     | exponent == 0 = return
-    | exponent `mod` 2 == 0 = powMod (base * base `mod` modulus) (exponent `div` 2) modulus return
+    | exponent `mod` 2 == 1 = powMod (base * base `mod` modulus) (exponent `div` 2) modulus return
     | otherwise = powMod (base * base `mod` modulus) (exponent `div` 2) modulus (return * base `mod` modulus)
---but this does
-modExp :: Integer -> Integer -> Integer -> Integer
-modExp  x y n = mod (x^(mod y (n-1))) (n)
---and this one too
-powm :: Integer -> Integer -> Integer -> Integer -> Integer
-powm b 0 m r = r
-powm b e m r
-  | e `mod` 2 == 1 = powm (b * b `mod` m) (e `div` 2) m (r * b `mod` m)
-powm b e m r = powm (b * b `mod` m) (e `div` 2) m r
